eb790af2b9327d6247d22a3b38fb628488e266ee
John Nyokabi <jwnyokabi@gmail.com>
2023-03-23 00:17:37 +0300

    AirBnB clone v2

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """getter attribute cities that returns the list of city"""
+        from models import storage
+        city_list = []
+        all_cities = storage.all(City).values()
+        for city in all_cities:
+            if self.id == city.state_id:
+                city_list.append(city)
+        return city_list
